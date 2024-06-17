e0b4358eae5d9e491f41600a76389e079df66d5d
Tim Kellogg <timothy.kellogg@gmail.com>
2024-06-17 14:31:33 -0400

    Added python generated code

--- scripts/codegen.py
+++ scripts/codegen.py
@@ -7,6 +7,10 @@ import pathlib
 
 from lab.models import Model, MODELS
 
+###############################################
+### Language: Go
+###############################################
+
 def do_go_model(model: Model, data: dict) -> str:
     def format_dim(dim: dict) -> str:
         return f"""{{Negative: "{dim["negative"]}", Positive: "{dim["positive"]}"}},"""
@@ -46,8 +50,55 @@ def do_go_model(model: Model, data: dict) -> str:
     else:
         return code
 
+
+###############################################
+### Language: Python
+###############################################
+
 def do_python_model(model: Model, data: dict) -> str:
-    pass
+    def format_dim(dim: dict) -> str:
+        return f"""DimLabel(negative="{dim["negative"]}", positive="{dim["positive"]}"),"""
+
+    def format_arr(weight_arr) -> str:
+        if isinstance(weight_arr[0], list):
+            weight_arr = weight_arr[0] 
+        buf = ["\n        ["]
+        for i, w in enumerate(weight_arr):
+            assert isinstance(w, float), type(w)
+            if i % 4 == 0:
+                buf.append("\n            ")
+            buf.append(f"{w},")
+        buf.append("\n        ],")
+        return "".join(buf)
+
+    dims = "\n        ".join(format_dim(d) for d in data["dims"])
+    weight_arr = data["weights"] if len(data["weights"]) == 3 else data["weights"][0]
+    weights = "\n        ".join(format_arr(w) for w in weight_arr)
+    proper_name = (
+        model.label
+        .lower()
+        .replace("(", "").replace(")", "")
+        .replace("-", "_")
+    )
+    code = f"""
+{proper_name} = EmoModel(
+	dims=[
+		{dims}
+	],
+	num_emb_dims={data["num_emb_dims"]},
+	weights=np.array([{weights}
+	]),
+)
+"""
+    if model.docs:
+        return f'{code}"""{model.docs}"""'
+    else:
+        return code
+
+
+###############################################
+### Main
+###############################################
 
 def get_model(path: pathlib.Path) -> Model | None:
     for model in MODELS:
@@ -57,16 +108,19 @@ def get_model(path: pathlib.Path) -> Model | None:
     return None
 
 def main():
-    code = []
+    go_code = []
+    python_code = []
     for file in pathlib.Path("output").glob("*.json"):
         model = get_model(file)
         if model is None:
             raise ValueError(f"Unknown model: {file}")
 
         emo_model_dict = json.loads(file.read_text())
-        code.append(do_go_model(model, emo_model_dict))
+        go_code.append(do_go_model(model, emo_model_dict))
+        python_code.append(do_python_model(model, emo_model_dict))
         print(f"{model.label}: {emo_model_dict is not None}")
-    pathlib.Path("go/emopoint/generated.go").write_text("package emopoint\n\n" + ("\n\n".join(code)))
+    pathlib.Path("go/emopoint/generated.go").write_text("package emopoint\n\n" + ("\n\n".join(go_code)))
+    pathlib.Path("emopoint/generated.py").write_text("import numpy as np\n\nfrom emopoint.lib import EmoModel, DimLabel\n\n" + ("\n\n".join(python_code)))
 
 if __name__ == "__main__":
     main()
